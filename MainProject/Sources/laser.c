73d9b4dc9cad635f5430dffbdae8603d1b154cf2
unknown <ivourgod@gmail.com>
2022-05-10 13:57:22 +1000

    Modified stewarts code to make it more robust

--- MainProject/Sources/laser.c
+++ MainProject/Sources/laser.c
@@ -7,17 +7,16 @@ unsigned long riseEdge = 0;
 unsigned long fallEdge = 0;
 unsigned long lastLaserSample = 0;
 
-void laserInit(void){
-                    
-  TSCR1_TEN = 1;   // enable the timers (this might happen more than once)
-
-  TIE_C1I = 1;     // enable interrupts for channel 1                         
+void laserInit(void) {
+                                             
   TIOS_IOS1 = 0;   // Set Laser ch1 to input capture
   
   TCTL4_EDG1A = 1;
   TCTL4_EDG1B = 1; // trigger on both rising and falling edge interrupts
 
+  TSCR1_TEN = 1;   // enable the timer (this might happen more than once)
   TFLG1 |= TFLG1_C1F_MASK; // Reset flag
+  TIE_C1I = 1;     // enable interrupts for channel 1
 }
 
 void GetLatestLaserSample(unsigned long *sample) {
@@ -32,14 +31,14 @@ __interrupt void TC1_ISR(void) {
   if (PTT_PTT1) { 
     riseEdge = (unsigned long)TC1;
   } 
-  else {
+  else {  //captured a falling edge
     fallEdge = (unsigned long)TC1;
-
-    if (fallEdge < riseEdge) {
-        fallEdge = (unsigned long)65536 + (unsigned long)fallEdge;
+    if (TFLG2) { //if timer overflowed
+      lastLaserSample =  65536 - riseEdge + fallEdge;
+      TFLG2 = 128;  //clearing TOF
     }
-
-    lastLaserSample = fallEdge - riseEdge; // Calculate the period of the PWM wave
+    else {       //timer did not overflow
+      LastLaserSample = fallEdge - riseEdge;
   }
   
   TFLG1 |= TFLG1_C1F_MASK; // Reset flag
