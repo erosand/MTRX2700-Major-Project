afa261a3425414fcd0207f30a006c94a9549318d
eanderbe <eanderbe@kth.se>
2022-05-22 15:16:28 +1000

    Updated struct formats and changed point cloud plot to top view

--- Python/serialCom.py
+++ Python/serialCom.py
@@ -4,9 +4,9 @@ import traceback
 import matplotlib.pyplot as plt
 import numpy as np
 
-MSG_HEADER_SIZE = 16
+MSG_HEADER_SIZE = 14
 
-#TODO: Add colorbar on the side
+#TODO: Add colorbar on the side?
 
 def readSerial(serialPort):   
     # Wait until there is data waiting in the serial buffer
@@ -14,9 +14,9 @@ def readSerial(serialPort):
         try:
             data = readPacket(serialPort)
 
-            if isinstance(data,list):
+            if isinstance(data,list): # If the packet was a point  message
                 return data
-            elif data: # If the packet was a text message nothing needs to be returned
+            elif data: # If the packet was a text or gyro message nothing needs to be returned
                 return False
             else:
                 print("Error, data = ", data)
@@ -33,26 +33,20 @@ def readPacket(serialPort):
         print("Error: Header is ", len(header_bytes), " bytes")
         return False
  
-    header_data = struct.unpack(">H8sHHH", header_bytes)
-    print("Header sentinels: " + str(hex(header_data[0])) + ", " + str(hex(header_data[4])))
+    header_data = struct.unpack(">H8sHH", header_bytes)
+    print("\nHeader sentinels: " + str(hex(header_data[0])) + ", " + str(hex(header_data[3])))
 
     message_type = header_data[1].split(b'\0', 1)[0]  # Remove the null characters from the string
-    print(message_type)
-    print("Message type: " + message_type.decode("utf-8"))
+    print("Message type: " + str(message_type))
     print("Message size: " + str(header_data[2]))
 
     if message_type == b"text":
         text_bytes = serialPort.read(header_data[2])
         print("Text message: " + str(text_bytes.decode("utf-8")))
         return True
-    elif message_type == b"gyro":
-        gyro_bytes = serialPort.read(header_data[2])
-        gyro_data = struct.unpack(">hhhhH", gyro_bytes)
-        print("Gyro message: " + str(gyro_data[1:4]) + ", time=" + str(gyro_data[4]))
-        return True
     elif message_type == b"point":
         point_bytes = serialPort.read(header_data[2])
-        point_data = struct.unpack(">iiiIi", point_bytes)
+        point_data = struct.unpack(">HhhIH", point_bytes)
         print("Point message: " + str(point_data[1:4]))
         return list(point_data[1:4])
     else: 
@@ -61,8 +55,8 @@ def readPacket(serialPort):
 
 def sendPoint(sp,point):
     point = [int(p) for p in point]
-    msg_point = struct.pack(">iiiIi", 0xAA, point[0], point[1], point[2], 0xBB)
-    header_point = struct.pack(">H8sHHH", 0xABCD, b"point", len(msg_point), 0, 0xDCBA)
+    msg_point = struct.pack(">HhhIH", 0xAA, point[0], point[1], point[2], 0xBB)
+    header_point = struct.pack(">H8sHH", 0xABCD, b"point", len(msg_point), 0xDCBA)
     sp.write(header_point)
     sp.write(msg_point)
 
@@ -74,34 +68,37 @@ def randomPoints(n = 200, az_lim = 45, el_lim = 45, dist_lim = [2000, 10000]):
     return points
 
 def sphe2cart(data,sensor_pos=[0,0,0]):
-    x = data[2]/1000*np.sin(np.radians(data[0])) + sensor_pos[0]
-    y = data[2]/1000*np.cos(np.radians(data[0])) + sensor_pos[1]
-    z = data[2]/1000*np.sin(np.radians(data[1])) + sensor_pos[2]
+    # x = data[2]/1000*np.sin(np.radians(data[0])) + sensor_pos[0] # Side view
+    # y = data[2]/1000*np.cos(np.radians(data[0])) + sensor_pos[1]
+    # z = data[2]/1000*np.sin(np.radians(data[1])) + sensor_pos[2]
+    x = data[2]/1000*np.sin(np.radians(data[0])) + sensor_pos[0] # Top view
+    y = data[2]/1000*np.sin(np.radians(data[1])) + sensor_pos[1]
+    z = sensor_pos[2] - data[2]/1000*np.cos(np.radians(data[0]))
     return [x,y,z]
 
 def figSetup(fig,params):
-    [sensor_pos, cl_width, cl_depth, cl_height] = params
+    [sensor_pos, win_x, win_y, win_z] = params
     ax = fig.add_subplot(111,projection='3d')    
     plt.show(block=False) # Lets the remaining code keep running
     ax.legend()
-    ax.set_xlabel('Width (x)')
-    ax.set_ylabel('Depth (y)')
-    ax.set_zlabel('Height (z)')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_zlabel('Z-axis')
     ax.set_title('Point cloud')
-    ax.set(xlim=(-cl_width/2+sensor_pos[0], cl_width/2+sensor_pos[0]), xticks=np.arange(-cl_width/2+sensor_pos[0], cl_width/2+1+sensor_pos[0]),
-            ylim=(sensor_pos[1], cl_depth+sensor_pos[1]), yticks=np.arange(sensor_pos[1], cl_depth+sensor_pos[1]+1),
-            zlim=(-cl_height/2+sensor_pos[2], cl_height/2+sensor_pos[2]), zticks=np.arange(-cl_height/2+sensor_pos[2], cl_height/2+1+sensor_pos[2]))
+    ax.set(xlim=(sensor_pos[0]-win_x/2, sensor_pos[0]+win_x/2), xticks=np.arange(sensor_pos[0]-win_x/2, sensor_pos[0]+win_x/2+1),
+            ylim=(sensor_pos[1]-win_y/2, sensor_pos[1]+win_y/2), yticks=np.arange(sensor_pos[1]-win_y/2, sensor_pos[1]+win_y/2+1),
+            zlim=(0, sensor_pos[2]), zticks=np.arange(0, sensor_pos[2]+1))
     return ax
 
 if __name__ == '__main__':
     sp1 = serial.Serial(port="COM1", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For sending (testing)
-    sp2 = serial.Serial(port="COM10", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For receiving
+    sp2 = serial.Serial(port="COM2", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For receiving
     
-    sensor_pos = [0, 0, 0]
-    cl_width = 10
-    cl_depth = 10
-    cl_height = 10
-    ax = figSetup(plt.figure(), [sensor_pos, cl_width, cl_depth, cl_height]) # Setup figure and plot sensor position
+    win_x = 10
+    win_y = 10
+    win_z = 10
+    sensor_pos = [win_x/2, win_y/2, win_z]
+    ax = figSetup(plt.figure(), [sensor_pos, win_x, win_y, win_z]) # Setup figure and plot sensor position
     ax.scatter(sensor_pos[0],sensor_pos[1],sensor_pos[2],c='r',marker='s',label='LIDAR')
 
     r_data = randomPoints(200, 45, 45, [2000,10000]) # Generate simulated points from the Lidar   
@@ -111,7 +108,7 @@ if __name__ == '__main__':
     plt.pause(1) # Needed for contiuous updates of the plot ("animation")
 
     while p_count < 100:
-       # sendPoint(sp1,r_data[i]) # Simulates data being sent from Lidar     
+        sendPoint(sp1,r_data[p_count]) # Simulates data being sent from Lidar     
         data = readSerial(sp2) 
         if data:
             data_vec.append(data)
@@ -119,9 +116,9 @@ if __name__ == '__main__':
             [x,y,z] = sphe2cart(data,sensor_pos) # Convert from spherical coordinates to cartesian
             points.append([x,y,z])
 
-            y_n = (y-1.4)/(cl_depth-1.4) # Normalise y (depth) to [0,1]
-            print(y,y_n)
-            col = [0.5*y_n, 1-y_n, y_n] # Calculate RGB color based on normalised y-value 
+            z_n = (sensor_pos[2] - z - 1.4)/(sensor_pos[2] - 1.4) # Normalise y (depth) to [0,1]
+            col = [0.5*z_n, 1-z_n, z_n] # Calculate RGB color based on normalised y-value 
+            print(z,)
             ax.scatter(x,y,z,s=4,depthshade=False,label='Points',color=col)
             ax.legend(['Lidar','Points'])
             
