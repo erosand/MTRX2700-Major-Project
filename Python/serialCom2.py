b9e94f86a598d0330494ba07081e965e0a1f6db4
eanderbe <eanderbe@kth.se>
2022-05-23 14:58:19 +1000

    New serial communication format

--- Python/serialCom2.py
+++ Python/serialCom2.py
@@ -4,14 +4,16 @@ import traceback
 import matplotlib.pyplot as plt
 import numpy as np
 
-MSG_HEADER_SIZE = 14
+MSG_SIZE = 14
 
 
 
 def readSerial(serialPort):   
-    if serialPort.in_waiting > 0:
+    if serialPort.in_waiting:
         try:
             data = readPacket(serialPort)
+
+            return data
 
             if isinstance(data,list): # If the packet was a point  message
                 return data
@@ -25,39 +27,36 @@ def readSerial(serialPort):
             # Logs the error appropriately.     
                   
 def readPacket(serialPort):
-    header_bytes = serialPort.read(MSG_HEADER_SIZE)
+    in_bytes = serialPort.read(1)
+    return in_bytes
 
-    if len(header_bytes) < MSG_HEADER_SIZE:
-        # must be out of messages
-        print("Error: Header is ", len(header_bytes), " bytes")
-        return False
+    # if len(header_bytes) < MSG_HEADER_SIZE:
+    #     # must be out of messages
+    #     print("Error: Header is ", len(header_bytes), " bytes")
+    #     return False
  
-    header_data = struct.unpack(">H8sHH", header_bytes)
-    #print("\nHeader sentinels: " + str(hex(header_data[0])) + ", " + str(hex(header_data[3])))
+    # header_data = struct.unpack(">H8sHH", header_bytes)
+    # #print("\nHeader sentinels: " + str(hex(header_data[0])) + ", " + str(hex(header_data[3])))
 
-    message_type = header_data[1].split(b'\0', 1)[0]  # Remove the null characters from the string
-    # print("Message type: " + str(message_type))
-    # print("Message size: " + str(header_data[2]))
+    # message_type = header_data[1].split(b'\0', 1)[0]  # Remove the null characters from the string
+    # # print("Message type: " + str(message_type))
+    # # print("Message size: " + str(header_data[2]))
 
-    if message_type == b"text":
-        text_bytes = serialPort.read(header_data[2])
-        #print("Text message: " + str(text_bytes.decode("utf-8")))
-        return True
-    elif message_type == b"point":
-        point_bytes = serialPort.read(header_data[2])
-        point_data = struct.unpack(">HhhIH", point_bytes)
-        #print("Point message: " + str(point_data[1:4]))
-        return list(point_data[1:4])
-    else: 
-        print("Error: Invalid message type: ", message_type,"\n\n")
-        return False
+    # if message_type == b"text":
+    #     text_bytes = serialPort.read(header_data[2])
+    #     #print("Text message: " + str(text_bytes.decode("utf-8")))
+    #     return True
+    # elif message_type == b"point":
+    #     point_bytes = serialPort.read(header_data[2])
+    #     point_data = struct.unpack(">HhhIH", point_bytes)
+    #     #print("Point message: " + str(point_data[1:4]))
+    #     return list(point_data[1:4])
+    # else: 
+    #     print("Error: Invalid message type: ", message_type,"\n\n")
+    #     return False
 
-def sendPoint(sp,point):
-    point = [int(p) for p in point]
-    msg_point = struct.pack(">HhhIH", 0xAA, point[0], point[1], point[2], 0xBB)
-    header_point = struct.pack(">H8sHH", 0xABCD, b"point", len(msg_point), 0xDCBA)
-    sp.write(header_point)
-    sp.write(msg_point)
+def sendByte(sp,b):
+    sp.write(b)
 
 def randomPoints(n = 200, az_lim = 45, el_lim = 45, dist_lim = [2000, 10000]):
     az = np.random.uniform(low=-az_lim,high=az_lim,size=n)
@@ -90,8 +89,8 @@ def figSetup(fig,params):
     return ax
 
 if __name__ == '__main__':
-    sp1 = serial.Serial(port="COM1", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For sending (testing)
-    sp2 = serial.Serial(port="COM2", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For receiving
+    # sp1 = serial.Serial(port="COM1", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For sending (testing)
+    sp2 = serial.Serial(port="COM10", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE) # For receiving
     
     win_x = 10
     win_y = 10
@@ -101,20 +100,35 @@ if __name__ == '__main__':
     ax.scatter(sensor_pos[0],sensor_pos[1],sensor_pos[2],c='r',marker='s',label='LIDAR')
 
     n_points = 100
-    r_data = randomPoints(n_points, 45, 45, [2000,10000]) # Generate simulated points from the Lidar   
+    #r_data = randomPoints(n_points, 45, 45, [2000,10000]) # Generate simulated points from the Lidar   
     data_vec = []
+    data = 0
     points = []
     p_count = 0
     plt.pause(1) # Needed for contiuous updates of the plot ("animation")
 
+    testdata = b'Point,111,11,12345\nPoint,22,222,12345\n'
+    
+    in_bytes = b''
+
     while p_count < n_points:
-        sendPoint(sp1,r_data[p_count]) # Simulates data being sent from Lidar     
-        data = readSerial(sp2) 
+        # a = testdata[i].to_bytes(1,'big')
+        # sp1.write(a)
+
+        if sp2.in_waiting:
+            in_bytes = in_bytes + sp2.read(1)
+            if in_bytes[-1].to_bytes(1,'big') == b'\n':
+                byte_msg = in_bytes
+                in_bytes = b''
+                datalist = byte_msg.decode('utf-8').strip('\n').split(',')[1:]
+                data = [int(el) for el in datalist]
+                print(data)
+
         if data:
             data_vec.append(data)
-
             [x,y,z] = sphe2cart(data,sensor_pos) # Convert from spherical coordinates to cartesian
             points.append([x,y,z])
+            data = 0
 
             z_n = (sensor_pos[2] - z - 1.4)/(sensor_pos[2] - 1.4) # Normalise y (depth) to [0,1]
             col = [0.5*z_n, 1-z_n, z_n] # Calculate RGB color based on normalised y-value 
@@ -122,6 +136,9 @@ if __name__ == '__main__':
             ax.legend(['Lidar','Points'])
             ax.set_title('Point cloud, ' + str(p_count+1) + ' points')
             plt.pause(0.1) # Needed for contiuous updates of the plot ("animation")
+
             p_count = p_count + 1
+        print(p_count)
 
     plt.show(block=True) # Prevents the figure from closing when the program is finished
+    
